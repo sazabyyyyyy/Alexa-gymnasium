a2807e06ff69635d0fdcb72c5d866c4040554950
kinko <starfish1138jp@gmail.com>
2019-12-08 17:18:45 +0900

    休館日取得Script作成

---
+++ js/getDays.js
@@ -0,0 +1,48 @@
+const puppeteer = require("puppeteer");
+const fs = require('fs');
+
+async function test() {
+
+    //ヘッドレスモードでページ開く
+    const browser=await puppeteer.launch();
+    const page =await browser.newPage();
+
+    const URL='https://www.shsf.jp/chuo-g';
+
+
+    //URLアクセス
+    await page.goto(URL,{
+        waitUntil: 'domcontentloaded'
+    });
+
+
+    //要素取得
+    const tables=await page.$$('.even');
+
+    for(const table of tables){
+        const data = await table.$('.data');
+        date = await (await data.getProperty('textContent')).jsonValue();
+    }
+
+    //Chromunium終了
+    await page.close();
+
+
+    //jsonファイル作成
+    const obj = {
+        days:date
+    };
+
+    const json=JSON.stringify(obj);
+
+    fs.writeFile('day.json',json,(err)=> {
+        if (err) console.log(`error!::${err}`);
+    });
+}
+
+
+(async ()=> {
+
+   test();
+
+})();
